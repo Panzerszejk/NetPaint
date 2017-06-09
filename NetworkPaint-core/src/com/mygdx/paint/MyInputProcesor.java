33e38d08431a2b023f331500c58f71009911923e
Panzerszejk <bober.konrad@student.pk.edu.pl>
2017-06-09 16:43:23 +0200

    ShapeRenderer
    Zmiana rysowania na Shape Renderer

--- NetworkPaint-core/src/com/mygdx/paint/MyInputProcesor.java
+++ NetworkPaint-core/src/com/mygdx/paint/MyInputProcesor.java
@@ -1,9 +1,95 @@
+
 package com.mygdx.paint;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Buttons;
+
+public class MyInputProcesor implements InputProcessor {
+    private boolean IsKeyDown=false;
+    private Queue<Integer[]> fifo = new LinkedList<Integer[]>();
+    Integer[] tab=new Integer[3];
+    
+    public Integer[] pollFifo () {
+    	Integer[] temp=new Integer[3];
+    	temp=fifo.poll();
+        return temp;
+    }
+    
+    
+	@Override
+    public boolean keyDown (int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp (int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped (char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown (int x, int y, int pointer, int button) {
+    	if(button == Buttons.LEFT) {
+    		tab[0]=x;
+        	tab[1]=y;
+        	tab[2]=2;
+        	fifo.add(tab);
+    	}
+    	
+    	if(button == Buttons.RIGHT) {
+    		
+    	}
+        return false;
+    }
+
+    @Override
+    public boolean touchUp (int x, int y, int pointer, int button) {
+    		tab[0]=x;
+    		tab[1]=y;
+    		tab[2]=0;
+    		fifo.add(tab);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged (int x, int y, int pointer) {
+    	tab[0]=x;
+    	tab[1]=y;
+    	tab[2]=1;
+    	fifo.add(tab);
+    	
+    	return false;
+    }
+
+    @Override
+    public boolean scrolled (int amount) {
+        return false;
+    }
+
+	@Override
+	public boolean mouseMoved(int arg0, int arg1) {	
+		return false;
+	}
+}
+
+/*
+package com.mygdx.paint;
+
 import com.badlogic.gdx.InputProcessor;
 
 public class MyInputProcesor implements InputProcessor {
+	public int[] Xbuffer;
+	public int[] Ybuffer;
+	public boolean[] Sbuffer;
+	public int bufferPointer;
+	
     @Override
     public boolean keyDown (int keycode) {
         return false;
@@ -21,18 +107,27 @@ public class MyInputProcesor implements InputProcessor {
 
     @Override
     public boolean touchDown (int x, int y, int pointer, int button) {
-    	System.out.println("Wcisniety");
+    	bufferPointer++;
+    	Sbuffer[bufferPointer] = false;
+    	Xbuffer[bufferPointer] = x;
+    	Ybuffer[bufferPointer] = y;
         return false;
     }
 
     @Override
     public boolean touchUp (int x, int y, int pointer, int button) {
-    	System.out.println("Upuszczony");
+    	Sbuffer[bufferPointer] = false;
+    	Xbuffer[bufferPointer] = x;
+    	Ybuffer[bufferPointer] = y;
         return false;
     }
 
     @Override
     public boolean touchDragged (int x, int y, int pointer) {
+    	bufferPointer++;
+    	Sbuffer[bufferPointer] = true;
+    	Xbuffer[bufferPointer] = x;
+    	Ybuffer[bufferPointer] = y;
         return false;
     }
 
@@ -47,3 +142,4 @@ public class MyInputProcesor implements InputProcessor {
 		return false;
 	}
 }
+*/
--- NetworkPaint-core/src/com/mygdx/paint/NetworkPaint.java
+++ NetworkPaint-core/src/com/mygdx/paint/NetworkPaint.java
@@ -3,93 +3,94 @@ package com.mygdx.paint;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
-public class NetworkPaint extends ApplicationAdapter implements ApplicationListener {
-	SpriteBatch batch;
-	private Pixmap pixmap;
-	private Sprite sprite;
-	Texture texture;
+public class NetworkPaint extends ApplicationAdapter {
+	public OrthographicCamera camera;
+	ShapeRenderer shapeRenderer;
+	public int width;
+	public int height;
+	int brushSize;
+	private MyInputProcesor inputProcesor;	//inputProcesor globalny na cala klase, zeby uzywac
+											//jego pol (isPressed) do decydowania o rysowaniu
+	
+	Integer[] PosTab;  //tablica pozycji obecnej
+	Integer[] LastTab;	//tablica pozycji poprzedniej
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();		
+        inputProcesor = new MyInputProcesor();	//utworzenie procesora obslugi wejsc
+        Gdx.input.setInputProcessor(inputProcesor);	//ustawienie procesora wejsc naszego gdx
 		
-		pixmap = new Pixmap(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(), Pixmap.Format.RGBA8888);
-        //Fill it red
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        
-        //Draw two lines forming an X
-        pixmap.setColor(Color.BLACK);
-        //pixmap.drawLine(0, 0, pixmap.getWidth()-1, pixmap.getHeight()-1);
-        //pixmap.drawLine(0, pixmap.getHeight()-1, pixmap.getWidth()-1, 0);
-        
-        //pixmap.setColor(Color.YELLOW);
-        //pixmap.drawCircle(pixmap.getWidth()/2, pixmap.getHeight()/2, pixmap.getHeight()/2 - 1);
-        MyInputProcesor inputProcessor = new MyInputProcesor();
-        Gdx.input.setInputProcessor(inputProcessor);
-        
-        texture = new Texture(pixmap);
-        
-        //pixmap.dispose();
-        
-        sprite = new Sprite(texture);
-        sprite.setPosition(0,0);
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, width, height);
+		camera.update();
+		shapeRenderer = new ShapeRenderer();
+		Gdx.graphics.setContinuousRendering(false);
+		brushSize = 10;
+		
+		
+		PosTab= new Integer[3];
+        LastTab= new Integer[2];
+        		
+        PosTab=null;
+        LastTab=null;
 	}
 
 	@Override
 	public void render () {
+		PosTab=inputProcesor.pollFifo();  //Metoda zdejmuje ostatni element z listy fifo. Jesli elementow nie ma zwraca null
+		
 		Gdx.gl.glClearColor(255, 255, 255, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
-        /*if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                sprite.translateX(-1f);
-            else
-                sprite.translateX(-10.0f);
+        if(PosTab!=null){ 		//Musimy sprawdzic czy przypadkiem PosTab nie jest pusty(null) bo inaczej wywali nam NullPointerException
+	        if(PosTab[2]==1) {
+	        	if(LastTab==null) {
+	        		LastTab= new Integer[2];
+	            	LastTab[0]=PosTab[0];
+	        		LastTab[1]=PosTab[1];
+	            }
+	        	else {
+	                camera.update();
+	                int x1, x2, y1, y2;
+	        	    shapeRenderer.begin(ShapeType.Filled);
+	                shapeRenderer.setColor(290/255f, 190/255f,190/255f, 1);
+	        		
+	        		
+		        	x1 = LastTab[0];
+		      		x2 = PosTab[0];
+		      		y1 = height - LastTab[1];
+		      		y2 = height - PosTab[1];
+		      		
+		      		shapeRenderer.circle(x1,y1,brushSize/2);
+		      		shapeRenderer.rectLine(x1,y1,x2,y2,brushSize);
+		      		
+		      		LastTab[0]=PosTab[0];
+	        		LastTab[1]=PosTab[1];
+	        		
+	        		shapeRenderer.end();
+	        	}
+	        }
+	        else {
+	        	LastTab=null; //ciekawe ale jak przypiszemy null to nie musimy usowac obiektu. Robi to za nas garbage colector po jakims czasie
+	        }
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                sprite.translateX(1f);
-            else
-                sprite.translateX(10.0f);
-        }*/
-        
-        if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-            //sprite.setPosition(Gdx.input.getX() - sprite.getWidth()/2,
-                   // Gdx.graphics.getHeight() - Gdx.input.getY() - sprite.getHeight()/2);
-        	pixmap.drawPixel(Gdx.input.getX(), Gdx.input.getY());
-        	pixmap.drawCircle(Gdx.input.getX(), Gdx.input.getY(),1);
-        	texture.dispose();
-            texture = new Texture(pixmap);
-            sprite.setTexture(texture);
-        	
-        }
-        if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
-        	pixmap.drawPixel(Gdx.input.getX(), Gdx.input.getY());
-        	pixmap.drawCircle(Gdx.input.getX(), Gdx.input.getY(),1);
-        	//texture.dispose();
-            texture.draw(pixmap, 0, 0);
-            sprite.setTexture(texture);
-        }
-        
-        batch.begin();
-        sprite.draw(batch);
-        batch.end();
+
+    
 	}
+	
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		texture.dispose();
 	}
 }
-
-
