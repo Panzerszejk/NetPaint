5194d35e85a7ead0229949509e5c26750a4886c5
Panzerszejk <bober.konrad@student.pk.edu.pl>
2017-06-09 16:47:29 +0200

    Usuniecie komentarzy
    Zapomniałem wywalić niepotrzebne komentarze

--- NetworkPaint-core/src/com/mygdx/paint/MyInputProcesor.java
+++ NetworkPaint-core/src/com/mygdx/paint/MyInputProcesor.java
@@ -1,4 +1,3 @@
-
 package com.mygdx.paint;
 
 import java.util.LinkedList;
@@ -78,68 +77,3 @@ public class MyInputProcesor implements InputProcessor {
 		return false;
 	}
 }
-
-/*
-package com.mygdx.paint;
-
-import com.badlogic.gdx.InputProcessor;
-
-public class MyInputProcesor implements InputProcessor {
-	public int[] Xbuffer;
-	public int[] Ybuffer;
-	public boolean[] Sbuffer;
-	public int bufferPointer;
-	
-    @Override
-    public boolean keyDown (int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp (int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped (char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown (int x, int y, int pointer, int button) {
-    	bufferPointer++;
-    	Sbuffer[bufferPointer] = false;
-    	Xbuffer[bufferPointer] = x;
-    	Ybuffer[bufferPointer] = y;
-        return false;
-    }
-
-    @Override
-    public boolean touchUp (int x, int y, int pointer, int button) {
-    	Sbuffer[bufferPointer] = false;
-    	Xbuffer[bufferPointer] = x;
-    	Ybuffer[bufferPointer] = y;
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged (int x, int y, int pointer) {
-    	bufferPointer++;
-    	Sbuffer[bufferPointer] = true;
-    	Xbuffer[bufferPointer] = x;
-    	Ybuffer[bufferPointer] = y;
-        return false;
-    }
-
-    @Override
-    public boolean scrolled (int amount) {
-        return false;
-    }
-
-	@Override
-	public boolean mouseMoved(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-}
-*/
